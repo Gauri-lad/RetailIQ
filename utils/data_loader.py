0187eb8bfa6ec1e10d9e5c50b671f91c34607eae
Gauri Sanjay Lad <gaurilad90@gmail.com>
2026-07-12 20:48:06 +0530

    Fix datetime parsing

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,13 +7,17 @@ def load_data():
 
     df["Order Date"] = pd.to_datetime(
         df["Order Date"],
-        dayfirst=True
+        errors="coerce",
+        format="mixed"
     )
 
     df["Ship Date"] = pd.to_datetime(
         df["Ship Date"],
-        dayfirst=True
+        errors="coerce",
+        format="mixed"
     )
+
+    df = df.dropna(subset=["Order Date"])
 
     df["Shipping Days"] = (
         df["Ship Date"] - df["Order Date"]
