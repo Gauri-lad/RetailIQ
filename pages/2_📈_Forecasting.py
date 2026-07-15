03d52b7d438659c255dbf7cfe1bb9b0c14e28e86
Gauri Sanjay Lad <gaurilad90@gmail.com>
2026-07-15 14:12:18 +0530

    Fix monthly frequency

--- pages/2_📈_Forecasting.py
+++ pages/2_📈_Forecasting.py
@@ -31,7 +31,7 @@ sales_df["Order Date"] = pd.to_datetime(sales_df["Order Date"])
 # Create monthly actual sales
 monthly_sales = (
     sales_df
-    .groupby(pd.Grouper(key="Order Date", freq="ME"))["Sales"]
+    .groupby(pd.Grouper(key="Order Date", freq="MS"))["Sales"]
     .sum()
     .reset_index()
 )
